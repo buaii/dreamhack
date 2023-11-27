382cce73c4d82e26852f6946c96941e3539bdcdd
buaii <129252044+buaii@users.noreply.github.com>
2023-11-27 14:57:24 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,20 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 
-// 파일을 읽어오는 예제
-fs.readFile('flag.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
+// 파일 경로
+const filePath = 'flag.txt';
+
+// 파일을 읽어오는 함수
+async function readFile() {
+    try {
+        // 파일 읽기
+        const data = await fs.readFile(filePath, 'utf8');
+
+        // 파일 내용 출력
+        console.log('파일 내용:', data);
+    } catch (err) {
+        console.error('파일을 읽는 도중 오류가 발생했습니다.', err);
     }
-    console.log(data);
-});
+}
+
+// 함수 호출
+readFile();
