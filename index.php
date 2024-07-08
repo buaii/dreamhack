f40ed155d1b32e1e5aa244c076fa48a93bdf953d
buaii <129252044+buaii@users.noreply.github.com>
2024-07-08 14:33:41 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+// Filename: hacked.php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hacked by Buaii</title>
+    <style>
+        body {
+            background-color: black;
+            color: red;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: Arial, sans-serif;
+            font-size: 3em;
+        }
+    </style>
+</head>
+<body>
+    <div>Hacked by Buaii</div>
+</body>
+</html>
